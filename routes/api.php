eb23f21ee2d278dae3489becc8bc95d673be6d9b
Andrey Rodionov <xyz-c@mail.ru>
2024-05-13 22:12:39 +0300

    000

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/test', function () {
+Route::get('test', function () {
     echo "api ok";
 });
 
