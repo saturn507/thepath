e14ef6d51501a0b31e170d513d4e128c7374cf00
Andrey Rodionov <xyz-c@mail.ru>
2024-05-18 11:26:06 +0300

    000

--- database/migrations/2024_05_14_175547_tg_log_migration.php
+++ database/migrations/2024_05_14_175547_tg_log_migration.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->bigInteger('chat_id');
             $table->text('text')->nullable();
             $table->text('log')->nullable();
+            $table->boolean('act')->default(true);
             $table->timestamps();
         });
     }
