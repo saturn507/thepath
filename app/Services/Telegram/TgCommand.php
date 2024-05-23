df879ddb9736d51987144619feda6712d1e1ecf1
Andrey Rodionov <xyz-c@mail.ru>
2024-05-23 11:47:20 +0300

    000

--- app/Services/Telegram/TgCommand.php
+++ app/Services/Telegram/TgCommand.php
@@ -18,7 +18,7 @@ class TgCommand
     {
         match ($this->data['command']) {
             'new_game' => (new NewGameCommand($this->data))->run(),
-            'my_team' => return,
+            'my_team' => $this->missCommand(),
             default => $this->missCommand(),
         };
 
