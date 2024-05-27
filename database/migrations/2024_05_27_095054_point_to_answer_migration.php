2bd77609f43ff29e3c1a21a44ce0a707b979dc53
Родионов Андрей Олегович <rodia@gkomega.ru>
2024-05-27 12:59:12 +0300

    000

---
+++ database/migrations/2024_05_27_095054_point_to_answer_migration.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('point_to_answers', function (Blueprint $table) {
+            $table->foreignId('point_id')
+                ->constrained('points')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->string('answer')->nullable(false);
+            $table->string('answer_transformation')->nullable(false);
+
+            $table->index('point_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('point_to_answers');
+    }
+};
