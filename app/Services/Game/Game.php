b86f4dcc9ee1e9ab5a72d5532d46509d7765d47b
Родионов Андрей Олегович <rodia@gkomega.ru>
2024-05-31 12:48:02 +0300

    000

---
+++ app/Services/Game/Game.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Services\Game;
+
+use App\Helpers\AnswerHelper;
+use App\Models\Point;
+use Illuminate\Database\Eloquent\Builder;
+use App\Models\Game as GameModel;
+
+class Game
+{
+    public static function checkCurrentGameFromUser($userId)
+    {
+        $currentGame = GameModel::query()
+            ->with('questionLine')
+            ->whereHas(
+                'users',
+                fn(Builder $builder) => $builder->where('id', $userId)
+            )
+            ->where('act', true)
+            ->whereNull('finish_at')
+            ->first();
+
+        if($currentGame)
+            return $currentGame;
+
+        return null;
+    }
+
+    public static function checkAnswer($pointId, $answer)
+    {
+        return Point::query()
+            ->where('id', $pointId)
+            ->whereHas(
+                'answers',
+                fn(Builder $builder) => $builder->where('answer_transformation', AnswerHelper::low($answer))
+            )
+            ->exists();
+    }
+}
