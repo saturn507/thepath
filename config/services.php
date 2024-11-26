c0b9a61d85ae340746be62f8b85fb7c9c8b883ba
Andrey Rodionov <xyz-c@mail.ru>
2024-11-26 22:00:20 +0300

    000

--- config/services.php
+++ config/services.php
@@ -34,7 +34,7 @@ return [
     'yandex' => [
         'client_id' => env('OAUTH_YANDEX_CLIENT_ID', 'c88fb192c63d439d98fa3266dd5284aa'),
         'client_secret' => env('OAUTH_YANDEX_CLIENT_SECRET', '191141525af74ee1a9388ce3fa161d4b'),
-        'redirect' => 'http://thepath.local/auth/yandex/callback',
+        'redirect' => 'https://thepath.fun/backend/auth/yandex/callback',
     ],
 
     'vkontakte' => [
