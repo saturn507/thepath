befa9f45f77e149f6ea7b0976a6b2daf1fa30664
saturn507 <root@p519515.kvmvps>
2025-03-31 09:47:09 +0300

    000

--- config/telegram.php
+++ config/telegram.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    "token" => env('TG_BOT_TOKEN', '7178639784:AAGbpIsLVJqVQMGdE3Bd0oO6UrDhj-2vYyk'),
+    "token" => env('TG_BOT_TOKEN', ''),
     "quest_lines_view_count" => env('TG_BOT_QUESTION_LINES_VIEW_COUNT', 4),
 ];
